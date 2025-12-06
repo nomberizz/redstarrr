a663f0a0188aaf12d2c37009e2fce01b4124ce12
nomberizz <grace262025@gmail.com>
2025-12-06 18:30:39 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,7 +2,7 @@
 let cam;              
 let targetColor;      
 // ⭐ 색상 인식 복구: threshold를 250으로 충분히 높여서 텍스트가 뜨도록 보장
-let threshold = 215;    
+let threshold = 210;    
 // 성능 최적화: checkCellSize는 40으로 유지
 let checkCellSize = 20; 
 let textStep = 10;        
